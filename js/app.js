1c175566995ac0fa4be8ed42835ff01770a96f87
Brett Lyons <blyons@students.naropa.edu>
2015-11-13 15:41:23 -0700

    Users can add a new memory with a form

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
 angular.module('memoriesApp', [])
   .controller("MemoriesController", function($scope, $http) {
+    $scope.showMemForm = false;
     $scope.getMemories = function () {
-    return $http({
+    $http({
       method: 'GET',
       url: 'http://g12-brett-lyons-memories.cfapps.io/api/v1/memories'
     }).then(function successCallback(response) {
-      response.data.data.forEach(function(dataPoint) {
-        // console.log("SUCCESS", response.data.data);
-      })
+      console.log("RESPONSE", response, "RESPONSE.DATA", response.data)
       $scope.memoryResponse = response.data.data;
       }, function errorCallback(response) {
         console.log("ERROR:", response);
         $scope.msg = "ERR: " + response;
       });
     }();
+    $scope.toggleFormShow = function() {
+      $scope.showMemForm = !$scope.showMemForm;
+    }
+    $scope.postToApi = function () {
+      
+      console.log($scope.old_days, $scope.these_days, $scope.year);
+      const tmpObj = {
+          "data": {
+            "type": "memory",
+            "attributes": {
+              "old_days": $scope.old_days,
+              "these_days": $scope.these_days,
+              "year": $scope.year
+            }
+          }
+        }
+      $http.post('http://g12-brett-lyons-memories.cfapps.io/api/v1/memories', tmpObj).then ({
+        
+      })
+    };
   });
