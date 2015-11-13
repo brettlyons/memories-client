93929f536c5c72ef09088dfd7fef11bf3f03fbf9
Brett Lyons <blyons@students.naropa.edu>
2015-11-13 14:24:36 -0700

    Button to fetch memories from API added

---
+++ js/app.js
@@ -0,0 +1,17 @@
+angular.module('memoriesApp', [])
+  .controller("MemoriesController", function($scope, $http) {
+    $scope.getMemories = function () {
+    return $http({
+      method: 'GET',
+      url: 'http://g12-brett-lyons-memories.cfapps.io/api/v1/memories'
+    }).then(function successCallback(response) {
+      response.data.data.forEach(function(dataPoint) {
+        // console.log("SUCCESS", response.data.data);
+      })
+      $scope.memoryResponse = response.data.data;
+      }, function errorCallback(response) {
+        console.log("ERROR:", response);
+        $scope.msg = "ERR: " + response;
+      });
+    }
+  });
