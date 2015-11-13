fe7942545e80d5d8bba5d1f427d444a6b74edd2c
Brett Lyons <blyons@students.naropa.edu>
2015-11-13 16:26:19 -0700

    Years now sorting and unique only

--- js/app.js
+++ js/app.js
@@ -1,36 +1,47 @@
 angular.module('memoriesApp', [])
-  .controller("MemoriesController", function($scope, $http) {
+  .controller('MemoriesController', function($scope, $http) {
     $scope.showMemForm = false;
-    $scope.getMemories = function () {
+    $scope.getMemories = function() {
     $http({
       method: 'GET',
       url: 'http://g12-brett-lyons-memories.cfapps.io/api/v1/memories'
     }).then(function successCallback(response) {
-      console.log("RESPONSE", response, "RESPONSE.DATA", response.data)
+      // console.log('RESPONSE', response, 'RESPONSE.DATA', response.data)
       $scope.memoryResponse = response.data.data;
       }, function errorCallback(response) {
-        console.log("ERROR:", response);
-        $scope.msg = "ERR: " + response;
+        console.log('ERROR:', response);
+        $scope.msg = 'ERR: ' + response;
       });
-    }();
+    }(); // <-- essential auto-invoke on page load
+
     $scope.toggleFormShow = function() {
       $scope.showMemForm = !$scope.showMemForm;
-    }
-    $scope.postToApi = function () {
-      
+    };
+
+    $scope.postToApi = function() {
+
       console.log($scope.old_days, $scope.these_days, $scope.year);
       const tmpObj = {
-          "data": {
-            "type": "memory",
-            "attributes": {
-              "old_days": $scope.old_days,
-              "these_days": $scope.these_days,
-              "year": $scope.year
+        'data': {
+            'type': 'memory',
+            'attributes': {
+              'old_days': $scope.old_days,
+              'these_days': $scope.these_days,
+              'year': $scope.year
             }
           }
-        }
-      $http.post('http://g12-brett-lyons-memories.cfapps.io/api/v1/memories', tmpObj).then ({
-        
-      })
+      };
+      $http.post('http://g12-brett-lyons-memories.cfapps.io/api/v1/memories',
+                 tmpObj).then(function() {}); // just in case its needed later
     };
-  });
+    $http.get('http://g12-brett-lyons-memories.cfapps.io/api/v1/memories/years')
+      .then(function(response) {
+        $scope.sidebarYears = response.data.data.reduce(function(collection, currentVal) {
+          if (collection.indexOf(currentVal) < 0) {
+            collection.push(currentVal);
+          }
+          return collection;
+        }, []).sort(function(a,  b) { return a - b;});
+      });
+});
+
